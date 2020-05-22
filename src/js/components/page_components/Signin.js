e75821e9de1e64b51c862aef82a1fce4395f1c80
Jefferson C. Bernard <jefferson_bernard@brown.edu>
2020-05-22 12:51:52 -0400

    signin page styled without font

--- src/js/components/page_components/Signin.js
+++ src/js/components/page_components/Signin.js
@@ -15,6 +15,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Redirect } from 'react-router-dom';
 import {signinUser} from "../../../actions";
 import {connect} from "react-redux";
+import {spacing} from "@material-ui/system"
+import backgroundImg from "../../../images/signin-background.jpg"
 
 function Copyright() {
     return (
@@ -31,34 +33,48 @@ function Copyright() {
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        height: '100vh',
-    },
-    image: {
-        backgroundImage: 'url(https://source.unsplash.com/random)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        width: '35%',
+        //float: 'right',
     },
     paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
+        padding: theme.spacing(1),
+        height: '35px',
+        width: '80%',
+        //textAlign: 'center',
+        backgroundColor: "#ffffff"
     },
 }));
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         height:'100%',
+//     },
+//     image: {
+//         backgroundImage: 'url(https://source.unsplash.com/random)',
+//         backgroundRepeat: 'no-repeat',
+//         backgroundColor:
+//             theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+//         backgroundSize: 'cover',
+//         backgroundPosition: 'center',
+//     },
+//     paper: {
+//         margin: theme.spacing(8, 4),
+//         display: 'flex',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//     },
+//     avatar: {
+//         margin: theme.spacing(1),
+//         backgroundColor: theme.palette.secondary.main,
+//     },
+//     form: {
+//         width: '100%', // Fix IE 11 issue.
+//         marginTop: theme.spacing(1),
+//     },
+//     submit: {
+//         margin: theme.spacing(3, 0, 2),
+//     },
+// }));
 
 function SignInSide(props) {
     const classes = useStyles();
@@ -107,78 +123,110 @@ function SignInSide(props) {
         return (<Redirect to={"home"}/>);
     }
     return (
-        <Grid container component="main" className={classes.root}>
-            <CssBaseline />
-            <Grid item xs={false} sm={4} md={7} className={classes.image} />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            value={email}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            value={password}
-                            onChange={handleChange}
-                        />
+        <div id={"signin-container"}>
+            <div className={classes.root} style={{marginLeft: '60%', marginTop: '20%', alignContent: 'center'}}>
+                <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                        <input id="email" name={"email"} onChange={handleChange} value={email} style={{border: "1px #000000", borderRadius: "5px"}} className={classes.paper} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <input type={"password"} id="password" name={"password"} onChange={handleChange} value={password} style={{border: "1px #000000", borderRadius: "5px"}} className={classes.paper} />
+                        <br /><br />
+                        <div style={{color: "#ffffff", marginLeft: "142px"}}>forgot Password?</div>
+                    </Grid>
+                    <Grid item xs={12} style={{marginLeft: "22.85%"}}>
+                        <Button onClick={handleSubmit} disabled={email === "" || password === ""} style={{backgroundColor: "#ffffff", align: "center", paddingLeft: "50px", paddingRight: "50px", paddingTop: "10px", paddingBottom: "10px"}}>
+                            Signin
+                        </Button><br />
                         {errorMsg !== "" ? <div>{errorMsg}</div> : ""}
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            disabled={email === "" || password === ""}
-                            onClick={handleSubmit}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="/#/signup" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                        <Box mt={5}>
-                            <Copyright />
-                        </Box>
-
-                </div>
-            </Grid>
-        </Grid>
+                    </Grid>
+                    {/*<Grid item xs={3}>*/}
+                    {/*    <Paper className={classes.paper}>xs=3</Paper>*/}
+                    {/*</Grid>*/}
+                    {/*<Grid item xs={3}>*/}
+                    {/*    <Paper className={classes.paper}>xs=3</Paper>*/}
+                    {/*</Grid>*/}
+                    {/*<Grid item xs={3}>*/}
+                    {/*    <Paper className={classes.paper}>xs=3</Paper>*/}
+                    {/*</Grid>*/}
+                    {/*<Grid item xs={3}>*/}
+                    {/*    <Paper className={classes.paper}>xs=3</Paper>*/}
+                    {/*</Grid>*/}
+                </Grid>
+            </div>
+        </div>
+        // <Grid container component="main" className={classes.root}>
+        //     <CssBaseline />
+        //     <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        //     <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        //         <div className={classes.paper}>
+        //             <Avatar className={classes.avatar}>
+        //                 <LockOutlinedIcon />
+        //             </Avatar>
+        //             <Typography component="h1" variant="h5">
+        //                 Sign in
+        //             </Typography>
+        //                 <TextField
+        //                     variant="outlined"
+        //                     margin="normal"
+        //                     required
+        //                     fullWidth
+        //                     id="email"
+        //                     label="Email Address"
+        //                     name="email"
+        //                     autoComplete="email"
+        //                     autoFocus
+        //                     value={email}
+        //                     onChange={handleChange}
+        //                 />
+        //                 <TextField
+        //                     variant="outlined"
+        //                     margin="normal"
+        //                     required
+        //                     fullWidth
+        //                     name="password"
+        //                     label="Password"
+        //                     type="password"
+        //                     id="password"
+        //                     autoComplete="current-password"
+        //                     value={password}
+        //                     onChange={handleChange}
+        //                 />
+        //                 {errorMsg !== "" ? <div>{errorMsg}</div> : ""}
+        //                 <FormControlLabel
+        //                     control={<Checkbox value="remember" color="primary" />}
+        //                     label="Remember me"
+        //                 />
+        //                 <Button
+        //                     type="submit"
+        //                     fullWidth
+        //                     variant="contained"
+        //                     color="primary"
+        //                     className={classes.submit}
+        //                     disabled={email === "" || password === ""}
+        //                     onClick={handleSubmit}
+        //                 >
+        //                     Sign In
+        //                 </Button>
+        //                 <Grid container>
+        //                     <Grid item xs>
+        //                         <Link href="#" variant="body2">
+        //                             Forgot password?
+        //                         </Link>
+        //                     </Grid>
+        //                     <Grid item>
+        //                         <Link href="/#/signup" variant="body2">
+        //                             {"Don't have an account? Sign Up"}
+        //                         </Link>
+        //                     </Grid>
+        //                 </Grid>
+        //                 <Box mt={5}>
+        //                     <Copyright />
+        //                 </Box>
+        //
+        //         </div>
+        //     </Grid>
+        // </Grid>
     );
 }
 
