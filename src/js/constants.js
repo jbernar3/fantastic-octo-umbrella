1949ea14f7e5a387db818e996b7bb83b62ff1cb5
Jefferson C. Bernard <jefferson_bernard@brown.edu>
2020-08-13 19:36:28 -0400

    changed ec2 instance public dns

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,2 +1,2 @@
-export const HOST_NAME = 'http://ec2-3-22-179-124.us-east-2.compute.amazonaws.com:3000/';
+export const HOST_NAME = 'http://ec2-3-129-226-15.us-east-2.compute.amazonaws.com:3000/';
 // export const HOST_NAME = 'http://localhost:3000/';
